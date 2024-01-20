30aa507f98a07805d01a2bf8e737928644c19768
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2024-01-20 20:27:04 +0530

    added variable check

--- app2/src/index.ts
+++ app2/src/index.ts
@@ -1,6 +1,13 @@
 import { server } from './server';
+import { config } from 'dotenv';
+
+config();
 
 const start = async () => {
+  if (!process.env.VARIABLE) {
+    throw new Error('VARIABLE not found');
+  }
+  
   server.listen(3000, () => {
     console.log('sever started on port 3000');
   });
